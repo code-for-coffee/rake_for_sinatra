8e46cd47ce2c3999772dc3de6e8a1a45a4f714bd
James Traver <jtraverjr@gmail.com>
2016-03-29 15:29:06 -0500

    das security added

--- app.rb
+++ app.rb
@@ -1,11 +1,39 @@
 require './models/account'
 
+enable :sessions
+
+# access/store/retrieve session information from session
+# this is a {}
+# the session {}
+
 Account.create({
-  :name => 'Testy',
-  :password_hash => BCrypt::Password.create('ajsajflsfjfljsffljsadlfj'),
-  :email => 'test@heyya.outkast'
+  :name => 'Victoria',
+  :password => 'puppy',
+  :email => 'v@ga.co'
 }).save
 
 get '/' do
   Account.all.to_json
+end
+
+get '/login/:pwd' do
+  @v = Account.create(:name => 'victoria', :password => 'puppy', :email => 'v@v.v')
+  session[:is_logged_in] = @v.has_valid_password(params[:pwd])
+  session[:username] = @v.name
+  binding.pry
+  {:logged_in => session[:is_logged_in]}.to_json
+end
+
+get '/mustbeloggedintosee' do
+  binding.pry
+  if session[:is_logged_in] == false || session[:is_logged_in] == nil
+    "SECRETSSSSSS only logged in can haz see"
+  else
+    "you rock"
+  end
+end
+
+get '/logout' do
+  session[:is_logged_in] = nil
+  "you have been logged out, peace out"
 end
--- models/account.rb
+++ models/account.rb
@@ -1,3 +1,30 @@
 class Account < ActiveRecord::Base
 
+  include BCrypt
+  # password setter/mutator
+  def password=(users_password)
+    self.password_hash = BCrypt::Password.create(users_password)
+  end
+
+  def password
+    BCrypt::Password.new(self.password_hash)
+  end
+
+  def has_valid_password(attempted_password)
+    @current_user = self
+    if (self.password == attempted_password)
+      return true
+    else
+      return false
+    end
+  end
+
+  # usage
+  # Account.create(
+  #   :password => params[:pwd],
+  #
+  # )
+
+  # password getter
+
 end
